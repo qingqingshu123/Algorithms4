2810f2736ede7510fa13c24a94d464caefac68b3
qingqingshu <qingqingshu1234@gmail.com>
2017-04-30 16:28:56 +0800

    Initial commit

---
+++ src/chapter1/part1/Prac1.java
@@ -0,0 +1,15 @@
+package chapter1.part1;
+
+/**
+ * Created by jixiaoqiang on 2017/4/30.
+ */
+
+
+public class Prac1 {
+    public static void main(String[] args) {
+
+    }
+    /*int a = 0;*/
+
+
+}
